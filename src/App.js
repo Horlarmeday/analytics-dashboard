db054b96e609fb48b45dd75edddef47550152b30
Mahmud <ajaomahmud@gmail.com>
2020-07-12 08:09:19 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./body.css";
+import "antd/dist/antd.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { Layout } from "antd";
+import cubejs from "@cubejs-client/core";
+import { CubeProvider } from "@cubejs-client/react";
+import client from "./graphql/client";
+import Header from "./components/Header";
+const API_URL = "http://localhost:4000";
+const CUBEJS_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTQzODA4ODgsImV4cCI6MTU5NDQ2NzI4OH0.pDKI1kJGEuPNV_gP_I6QFFL653JbNpktjw-LEZcspdE";
+const cubejsApi = cubejs(CUBEJS_TOKEN, {
+  apiUrl: `${API_URL}/cubejs-api/v1`
+});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppLayout = ({ children }) => (
+  <Layout
+    style={{
+      height: "100%"
+    }}
+  >
+    <Header />
+    <Layout.Content>{children}</Layout.Content>
+  </Layout>
+);
+
+const App = ({ children }) => (
+  <CubeProvider cubejsApi={cubejsApi}>
+    <ApolloProvider client={client}>
+      <AppLayout>{children}</AppLayout>
+    </ApolloProvider>
+  </CubeProvider>
+);
 
 export default App;
